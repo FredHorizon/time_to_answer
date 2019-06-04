974467c7bc06a9d3ea518a69174b8c142f119e5a
FredHorizon <fredoliveira.sys@gmail.com>
2019-06-04 18:54:42 +0000

    Protege os backoffices com Devise

--- app/controllers/admins_backoffice_controller.rb
+++ app/controllers/admins_backoffice_controller.rb
@@ -1,3 +1,5 @@
 class AdminsBackofficeController < ApplicationController
+    before_action :authenticate_admin! # no singular porque é o nome do model
+
     layout 'admins_backoffice' # ativa o uso desse layout
 end
--- app/controllers/profiles_backoffice_controller.rb
+++ app/controllers/profiles_backoffice_controller.rb
@@ -1,3 +1,5 @@
 class ProfilesBackofficeController < ApplicationController
+    before_action :authenticate_profile!
+
     layout 'profiles_backoffice'
 end
