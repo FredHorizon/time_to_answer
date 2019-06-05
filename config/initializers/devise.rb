bb2e19ec69c07f69f7f6bf02954755a96876619c
FredHorizon <fredoliveira.sys@gmail.com>
2019-06-05 19:43:41 +0000

    Cria os links de Logout para os perfis 'user' e 'admin'

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,8 @@ Devise.setup do |config|
 
   # Set this configuration to false if you want /users/sign_out to sign out
   # only the current scope. By default, Devise signs out all scopes.
-  # config.sign_out_all_scopes = true
+  # Evita que o logoff seja feito em todos os escopos. Faz somente na sessão atual.
+  config.sign_out_all_scopes = false
 
   # ==> Navigation configuration
   # Lists the formats that should be treated as navigational. Formats like
