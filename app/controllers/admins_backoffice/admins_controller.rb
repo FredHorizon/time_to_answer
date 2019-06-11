4aab96285c65ca19d74b9f84c684becebc6dcb7e
FredHorizon <fredoliveira.sys@gmail.com>
2019-06-11 19:02:18 +0000

    Ajusta as rotas dos admins

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,4 +2,8 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+
+  def edit
+    # ação para editar informações de admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
+
   namespace :site do
     get 'welcome/index'
   end
@@ -9,7 +7,8 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins, only: [:index] # Administradores
   end
   
   devise_for :admins
