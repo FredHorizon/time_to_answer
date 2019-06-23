df98351067de43579ac91b61fe79b99d4d3d6828
FredHorizon <fredoliveira.sys@gmail.com>
2019-06-23 00:42:34 +0000

    Especifica a inclusao de relacionamento(n:1)

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -3,7 +3,10 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 	before_action :get_subjects, only: [:new, :edit] # Padrão MVC
 	
 	def index
-		@questions = Question.all.order(:description).page(params[:page])
+		# Query com a questão incluindo os assuntos
+		@questions = Question.includes(:subject)
+							 .order(:description)
+							 .page(params[:page])
 	end
 	
 	def new
