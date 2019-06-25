e7fa352a4d609bfbf8cdb576036a8a544169db70
FredHorizon <fredoliveira.sys@gmail.com>
2019-06-25 23:03:17 +0000

    Configura o model question com nested attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :subject
+  has_many :answers # uma questão possui muitas respostas
+
+  accepts_nested_attributes_for :answers # a questão deve aceitar atributos das respostas
 end
