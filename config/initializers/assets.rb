f75fd38397813bb1c799fa415c5fbfda82d810c9
FredHorizon <fredoliveira.sys@gmail.com>
2019-06-28 21:04:00 +0000

    Implementa template login para User Devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,6 +16,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css
                                                   users_backoffice.js users_backoffice.css
                                                   admin_devise.js admin_devise.css
+                                                  user_devise.js user_devise.css
                                                 )
 # lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
