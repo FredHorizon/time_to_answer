8797fea764c2058468c687f37b6ce3c62d6d1d83
FredHorizon <fredoliveira.sys@gmail.com>
2019-07-02 02:45:51 +0000

    Configura a listagen de perguntas e respostas na index

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,7 @@ class Question < ApplicationRecord
   has_many :answers # uma questão possui muitas respostas
 
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  # Kaminari
+  paginates_per 5
 end
