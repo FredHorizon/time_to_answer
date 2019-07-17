d5bf4f10354a899a87af13336d2723a35e458a57
FredHorizon <fredoliveira.sys@gmail.com>
2019-07-17 00:54:45 +0000

    Implementa Active Storage para avatar do perfil

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module UsersBackofficeHelper
+	
+	# Para o avatar aparecer no perfil
+	def avatar_url
+		avatar = current_user.user_profile.avatar
+		avatar.attached? ? avatar : 'img.jpg' # avatar possui imagem anexada? se sim(?), avatar. Senão(:) usar img.jpg
+	end
+
 end
