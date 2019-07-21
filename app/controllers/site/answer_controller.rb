f0811a3cdfa46eef1fe8b14278b5130ee8fbcda2
FredHorizon <fredoliveira.sys@gmail.com>
2019-07-21 03:41:15 +0000

    Refatora o UserStatistic (2/2)

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,7 +1,7 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer_id])
-    UserStatistic.set_statistic(@answer, current_user, user_signed_in?) # user user_signed_in como parâmetro pra poder ser chamado do model.
+    UserStatistic.set_statistic(@answer, current_user)
   end
 
 end
--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -7,8 +7,8 @@ class UserStatistic < ApplicationRecord
   end
 
   # Class method
-  def self.set_statistic(answer, current_user, user_signed_in)
-    if user_signed_in # o devise só permite a chamada direta desse helper no controller ou view. Aqui no model dará erro, por isso ele precisa ir como parâmetro.
+  def self.set_statistic(answer, current_user)
+    if !!current_user # nega 2x <!!> pra que se transforme em um valor booleano, e assim ateste se o usuário está logado ou não
       user_statistic = UserStatistic.find_or_create_by(user: current_user)
       answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
       user_statistic.save
