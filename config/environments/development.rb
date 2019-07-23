9e89bd33152a4ddb47981dcf718683c10ba2e5b8
FredHorizon <fredoliveira.sys@gmail.com>
2019-07-23 00:38:18 +0000

    implementa recuperaçao de senha com Mailtrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,17 @@ Rails.application.configure do
   # Devise config email
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Mailtrap config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '180ffa294ae905',
+    :password => '5d258b3d83c24c',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
