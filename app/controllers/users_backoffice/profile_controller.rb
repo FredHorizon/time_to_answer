4ee482a810cf68a60832e47349ac3f7194d79c52
FredHorizon <fredoliveira.sys@gmail.com>
2019-07-24 17:42:42 +0000

    Corrige identação do profile_controller de user_backoffice

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -19,18 +19,18 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
 	private
 
-	def params_user
-	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-		user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
-  end
-
 	def set_user
 		@user = User.find(current_user.id)
 	end
+	
+	def params_user
+	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
+		user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
+  	end
 
 	def verify_password
-    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
-      params[:user].extract!(:password, :password_confirmation)
-    end
-  end
+		if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+		params[:user].extract!(:password, :password_confirmation)
+		end
+  	end
 end
